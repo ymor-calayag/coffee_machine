95b66898cf8dce8e8235ce1594be1940c494ce76
ymor-calayag <jeffreyymor.calayag@gmail.com>
2025-12-26 13:09:21 +0800

    add resource checker functionality

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ print("\nPlease type in your order.")
 def print_report():
     for resource in data.resources:
         print(f"{resource}: {data.resources[resource]}")
+    print(f"Earnings: ₱{machine_money}")
+
+def check_resources(order):
+    for resource in data.MENU[order]["ingredients"]:
+        if data.resources[resource] < data.MENU[order]["ingredients"][resource]:
+            return False
+    return True
 
 order = input("What is your order: ").lower()
 
@@ -33,16 +40,20 @@ order = input("What is your order: ").lower()
 
 user_money = float(input("Please insert your money: "))
 coffee_cost = data.MENU[order]["cost"]
+is_resource_enough = check_resources(order)
 
-if user_money > coffee_cost:
+if user_money > coffee_cost and is_resource_enough == True:
     print(f"\nHere is your ☕ {order}.\nHere is your change: ₱{user_money - coffee_cost}")
     machine_money += coffee_cost
     
     # for deducting machine resources based on coffee ingredient
     for resource in data.MENU[order]["ingredients"]:
         data.resources[resource] -= data.MENU[order]["ingredients"][resource]
-
+elif is_resource_enough == False:
+    print("Sorry, the machine doesn't have enough ingredients to make your coffee.")
 elif user_money < coffee_cost:
     print(f"\nYour money is not enough.\nMoney refunded.")
+
+print(data.resources)
 
 
